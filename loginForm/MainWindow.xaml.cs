e1d38048e73089a841932f05a2424568ae48401e
harutopia0 <tran67563@gmail.com>
2025-10-17 03:44:29 +0700

    sign in gui

--- loginForm/MainWindow.xaml.cs
+++ loginForm/MainWindow.xaml.cs
@@ -25,5 +25,40 @@ namespace loginForm
         {
             Application.Current.Shutdown();
         }
+
+        private void usernameTextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            usernameBorder.BorderThickness = new Thickness(3);
+            usernameBorder.CornerRadius = new CornerRadius(5);
+
+            string hexColor = "#4b91cf";
+            Color color = (Color)ColorConverter.ConvertFromString(hexColor);
+            SolidColorBrush brush = new SolidColorBrush(color);
+            usernameBorder.BorderBrush = brush;
+        }
+
+        private void usernameTextBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            usernameBorder.BorderThickness = new Thickness(1);
+            usernameBorder.CornerRadius = new CornerRadius(3);
+            usernameBorder.BorderBrush = Brushes.Gray;
+        }
+
+        private void passwordBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            passwordBorder.BorderThickness = new Thickness(3);
+            passwordBorder.CornerRadius = new CornerRadius(5);
+
+            string hexColor = "#4b91cf";
+            Color color = (Color)ColorConverter.ConvertFromString(hexColor);
+            passwordBorder.BorderBrush = new SolidColorBrush(color);
+        }
+
+        private void passwordBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            passwordBorder.BorderThickness = new Thickness(1);
+            passwordBorder.CornerRadius = new CornerRadius(3);
+            passwordBorder.BorderBrush = Brushes.Gray;
+        }
     }
 }
